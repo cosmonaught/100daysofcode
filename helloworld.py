b843913b7f3403302f19d7f9dc3e0a646670d0a4
Lauren <cosmonaught@users.noreply.github.com>
2018-05-08 21:03:29 -0700

    Day1: Hello World

---
+++ helloworld.py
@@ -0,0 +1,12 @@
+#printing as always the hello world statement
+print("Hello world, I am participating in #100daysofcode challenge!")
+#remembering variables
+a = 3
+#if/elif statement bc I want to add something extra
+if a == 3:
+    print("This should print if a == 3 as part of a simple if statement")
+#elif
+else:
+    print("This should work, this should not be printing")
+
+
